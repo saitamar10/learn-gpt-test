5d0e7f85a42d2d32f7bd0ee9a4f9b33245a4da55
saitamar10 <94067189+saitamar10@users.noreply.github.com>
2023-06-29 13:08:51 +0300

    Update request.ts

--- pages/api/request.ts
+++ pages/api/request.ts
@@ -39,7 +39,7 @@ export default async function handler(req: NextRequest) {
   };
 
   const payload = {
-    model: "gpt-3.5-turbo",
+    model: "gpt-3.5-turbo-16k-0613",
     messages: systemMessage ? [systemMessage, userMessage] : [userMessage],
     stream: true,
   };
